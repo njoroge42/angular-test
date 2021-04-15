e1ed261c989970e1e674c2ea0e28dc879dbd4d65
njoroge42 <njorogerobert420@gmail.com>
2021-04-15 11:32:16 +0300

    2 files modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,33 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import {products} from '../products';
-import {CartService} from '../cart.service';
-import {ActivatedRoute} from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { products } from "../products";
+import { CartService } from "../cart.service";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-product-details',
-  templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  selector: "app-product-details",
+  templateUrl: "./product-details.component.html",
+  styleUrls: ["./product-details.component.css"]
 })
 export class ProductDetailsComponent implements OnInit {
   product;
 
-  addToCart(product){
-    this.CartService.addToCart(product);
-    window.alert("product added.")
-
+  addToCart(product) {
+    this.cartService.addToCart(product);
+    window.alert("product added.");
   }
   constructor(
-    private route : ActivatedRoute,
-    private CartService :CartService
-   ) { 
-    ;
-  }
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) {}
 
   ngOnInit() {
     const routeParams = this.route.snapshot.paramMap;
-    const productIdFromRoute = Number(routeParams.get('productId'));
+    const productIdFromRoute = Number(routeParams.get("productId"));
 
-    this.product = products.find(product => product.id ===productIdFromRoute);
+    this.product = products.find(product => product.id === productIdFromRoute);
   }
-
 }
